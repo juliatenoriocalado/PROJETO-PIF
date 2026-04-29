90add02a91510930e38638a6674586e8a0b3551f
juliatenoriocalado <jtc@cesar.school>
2026-04-29 18:08:07 -0300

    FUNCIONA ALGUMA COISA!

--- CarnivalStory/main.c
+++ CarnivalStory/main.c
@@ -67,6 +67,8 @@ void InitGame(){
     //Coordenadas do jogador
 
     jogador.vida = 100;
+    jogador.no_chao = 1;
+    jogador.velocidade_pulo = 0;
 
     inimigo.vida = 500;
     inimigo.ativado = 1;
@@ -77,7 +79,7 @@ void InitGame(){
 
 //Define vida, posição e movimentos do personagem e do inimigo básicos
 
-void UpdateGame(){
+void AtualizarJogo(){
 
     switch (ModoDoJogo){
 
@@ -86,19 +88,25 @@ void UpdateGame(){
                 ModoDoJogo = tela_animacaoIntro;
             }
             
-        break;
+            break;
 
-        case tela_animacao:
+        case tela_animacaoIntro:
             if (IsKeyPressed(KEY_ENTER)){
                 ModoDoJogo = tela_batalha;
             }
+
+            break;
 
          case tela_batalha:
 
             if (IsKeyDown(KEY_RIGHT)) jogador.posicao.x += 2;
             if (IsKeyDown(KEY_LEFT)) jogador.posicao.x -= 2;
 
-            if (IsKeyDown(KEY_SPACE) && jogador.no_chao){
+            if (IsKeyPressed(KEY_A)){
+                inimigo.vida -= 10;
+            }
+
+            if (IsKeyPressed(KEY_SPACE) && jogador.no_chao){
 
                 jogador.velocidade_pulo = INTENSIDADE_PULO;
                 jogador.no_chao = 0;
@@ -114,23 +122,17 @@ void UpdateGame(){
             jogador.posicao.y += jogador.velocidade_pulo; //A velocidade do jogador muda a posiçaõ do jogador
             // <-----------         <------------
 
-            if (IsKeyDown(KEY_DOWN)) jogador.posicao.y -= 2;
-
             //X seria a coordenada que marca a horizontal (eixo das abcissas)
             //Y seria a coordenada que marca a vertical (eixo das ordenadas)
             //Quando a tecla da seta direita é pressionada, o personagem anda para a direita
             //Quando a tecla da seta esquerda é pressionada, o personagem dá uma recuada (por isso o negativo) e acaba indo para a esquerda
 
                 if (jogador.posicao.y >= COORDENADA_CHAO){ //Se o jogador estiver no chão (sob a coordenada do eixo Y)
-                jogador.posicao.y = COORDENADA_CHAO; //Trava o jogador no chão
-                jogador.velocidade_pulo = 0; //Não vai estar pulando, para a queda
-                jogador.no_chao = 1; //E a afirmação se torna verdadeira de que ele tá no chão, permitindo que ele pule de novo
+                    jogador.posicao.y = COORDENADA_CHAO; //Trava o jogador no chão
+                    jogador.velocidade_pulo = 0; //Não vai estar pulando, para a queda
+                    jogador.no_chao = 1; //E a afirmação se torna verdadeira de que ele tá no chão, permitindo que ele pule de novo
 
             }
-
-            break;
-
-        case tela_batalha:
 
         //Inimigo ataca
 
@@ -141,10 +143,71 @@ void UpdateGame(){
         //Fim da batalha
 
             if (inimigo.vida <= 0){
-                ModoDoJogo = tel
+                ModoDoJogo = tela_animacaoFinal;
             }
+
+            break;
 
     }
 
+}
 
+void DesenharJogo(){
+
+    switch (ModoDoJogo){
+        case tela_menu:
+            DrawText("PRESSIONE ENTER PARA COMEÇAR", 180,300,20,WHITE);
+            break;
+
+        case tela_animacaoIntro:
+            DrawText("CARNIVAL STORY", 250,80,40, RED);
+
+            DrawRectangle(80,140,640,280,BLACK);
+            DrawRectangleLines(80,140,640,280,WHITE);
+
+            DrawText("Rose é uma menina gentil, cheia de sonhos...", 110,450,20,WHITE);
+            DrawText("ENTER", 180,520,20,GRAY); //Para iniciar a batalha
+            break;
+
+        case tela_batalha:
+            DrawText("LUTE!", 350,50,30,RED);
+
+            DrawCircleV(jogador.posicao,20,BLUE);
+
+            DrawText(TextFormat("Jogador HP: %d", jogador.vida), 50,100,20,WHITE);
+            DrawText(TextFormat("Inimigo HP: %d", inimigo.vida), 50,130,20,RED);
+
+            DrawText("Pressione A para atacar", 50,200,20,WHITE);
+            break;
+
+    }
+
+}
+
+//========================================================================================================
+//                                       Looping Principal!
+//========================================================================================================
+
+int main(){
+
+    InitWindow(LARGURA_TELA,ALTURA_TELA,"Carnival Story");
+    SetTargetFPS(60); //Função do Raylib que define os frames por segundo, sem isso, poderia rodar rápido demais (dependendo do pc)
+
+    InitGame();
+
+    while (!WindowShouldClose()){ //Enquanto a janela NÃO estiver fechada, continue rodando o jogo
+
+        AtualizarJogo(); //Lógica: teclas, movimentos, vida, colisão
+
+        BeginDrawing(); //Função especial que marca o começo da parte do desenho da tela (desenha o frame)
+
+        ClearBackground(DARKGRAY); //Limpar tela
+
+        DesenharJogo(); //Desenha o menu, player, inimigo, textos etc.
+
+        EndDrawing(); //Função especial para terminar o desenho e mostrar a janela (termina de desenhar o frame para finalmente mostrar na tela)
+
+        //Tanto o BeginDrawing e o EndDrawing são necessários, porque o jogo desenha a tela várias vezez por segundo, tipo 60 FPS
+        //Tudo que for visual fica entre eles
+    }
 }
